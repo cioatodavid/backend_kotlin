02532eef02f13baaa91d919db5fb3ce5a8c04099
david <55272383+cioatodavid@users.noreply.github.com>
2023-10-18 20:27:00 -0300

    userController add
    💥need a database connection to test all endpoints (db config was on gitignore)

--- backend/src/main/kotlin/br/upf/schemaflow/service/UserService.kt
+++ backend/src/main/kotlin/br/upf/schemaflow/service/UserService.kt
@@ -3,21 +3,38 @@ package br.upf.schemaflow.service
 import br.upf.schemaflow.entity.UserEntity
 import br.upf.schemaflow.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
-class UserService(@Autowired val userRepository: UserRepository) {
+@Service
+class UserService() {
+    @Autowired
+    lateinit var userRepository: UserRepository
+
     fun createUser(user: UserEntity): UserEntity {
         return userRepository.save(user)
     }
 
     fun updateUser(user: UserEntity): UserEntity {
-        return userRepository.save(user)
+        if (userRepository.existsById(user.id)) {
+            return userRepository.save(user)
+        }
+        throw Exception("User not found")
     }
 
-    fun deleteUser(user: UserEntity) {
-        userRepository.delete(user)
+
+    fun deleteUser(user: UserEntity?) {
+        if (user != null) {
+            if (userRepository.existsById(user.id)) {
+                userRepository.deleteById(user.id)
+            } else {
+                throw Exception("User not found")
+            }
+        }
     }
 
-    fun findUserById(id: Long): UserEntity {
-        return userRepository.findById(id).get()
+
+    fun findUserById(id: Long): UserEntity? {
+        return userRepository.findById(id).orElse(null)
     }
+
 }
